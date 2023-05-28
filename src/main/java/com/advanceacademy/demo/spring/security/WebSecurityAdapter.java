4c2058e709619cb45c59b60a629b8e0bff832aca
Ayhan Selim <codewolf27@gmail.com>
2023-05-28 19:21:01 +0300

    project cleanup

--- src/main/java/com/advanceacademy/demo/spring/security/WebSecurityAdapter.java
+++ src/main/java/com/advanceacademy/demo/spring/security/WebSecurityAdapter.java
@@ -3,5 +3,7 @@ package com.advanceacademy.demo.spring.security;
 public class WebSecurityAdapter {
 
     private String password;
-          
+    private String token;
+    private int log_tries;
+
 }
